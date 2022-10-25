92a97e4d4916fba6b8a7e0217108d83491f0e761
Klim <naiberd669@bk.ru>
2022-10-26 00:58:49 +0300

    Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+string GetNumberAndShowPalidrome(int numberFunction)
+{
+    string str = Convert.ToString(numberFunction);
+    string result;
+    if (str.Length == 5)
+    {
+        if ((str[0] == str[4]) & (str[1] == str[3]))
+        {
+            result = "Число " + numberFunction + " является палиндоном";
+        }
+        else
+        {
+            result = "Число " + numberFunction + " не является палиндоном";
+        }
+    }
+    else
+    {
+        result = "Число " + numberFunction + " не пятизначное";
+    }
+    return result;
+}
+
+Console.WriteLine("Введите 5-ти значное число, наприме 12345");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = GetNumberAndShowPalidrome(number);
+Console.WriteLine(result);
