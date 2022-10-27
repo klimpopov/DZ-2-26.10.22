e074fd84caaac9fe9927a180104df679995dabf3
Klim <naiberd669@bk.ru>
2022-10-27 19:54:24 +0300

    форматирование Task_23

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -7,7 +7,7 @@
 
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-for (int i=1; i<=number; i++)
+for (int i = 1; i <= number; i++)
 {
-Console.Write(Math.Pow(i, 3) + " ");
+    Console.Write(Math.Pow(i, 3) + " ");
 }
