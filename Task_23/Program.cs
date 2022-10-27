da3cf70f76ec1d9110def1c7fef4a7c76bd023aa
Klim <naiberd669@bk.ru>
2022-10-27 19:42:00 +0300

    Task_21 have done

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=number; i++)
+{
+Console.Write(Math.Pow(i, 2) + " ");
+}
